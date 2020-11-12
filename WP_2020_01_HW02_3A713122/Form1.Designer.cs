e8acb4d9d436a1704408fd091b03bda81313b89a
Daniel <dog89070388@gmail.com>
2020-11-12 21:13:55 +0800

    加入專案檔案。

---
+++ WP_2020_01_HW02_3A713122/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WP_2020_01_HW02_3A713122
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(396, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 88);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "洗牌";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(441, 224);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(133, 177);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(60, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 80);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "順序";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(52, 161);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(211, 240);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(441, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 58);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "抽牌";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WP_2020_01_HW02_3A713122/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_HW02_3A713122.Properties;
+
+namespace WP_2020_01_HW02_3A713122
+{
+
+    public partial class Form1 : Form
+    {
+        List<int> poker;
+        List<Image> picty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Poker p = new Poker();
+
+            poker = p.GetPoker(52);
+
+            string msg = "";
+            for (int i = 0; i < poker.Count; i++)
+            {
+                msg += $"{poker[i]} ,";
+
+            }
+
+            richTextBox1.Text = msg + "\n";
+            nub = 0;
+
+        }  
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+            int nub = 0;
+            private void button2_Click(object sender, EventArgs e)
+            {
+
+                if (nub < 52)
+                {
+
+                    pictureBox1.Image = picty[poker[nub]];
+                    nub++;
+
+                }
+
+            }
+
+
+
+            private void Form1_Load(object sender, EventArgs e)
+        {
+            picty = new List<Image>();
+            picty.Add(Resources.p8);
+            picty.Add(Resources.p9);
+            picty.Add(Resources.p10);
+            picty.Add(Resources.p11);
+            picty.Add(Resources.p12);
+            picty.Add(Resources.p13);
+            picty.Add(Resources.p14);
+            picty.Add(Resources.p15);
+            picty.Add(Resources.p16);
+            picty.Add(Resources.p17);
+            picty.Add(Resources.p18);
+            picty.Add(Resources.p19);
+            picty.Add(Resources.p20);
+            picty.Add(Resources.p21);
+            picty.Add(Resources.p22);
+            picty.Add(Resources.p23);
+            picty.Add(Resources.p24);
+            picty.Add(Resources.p25);
+            picty.Add(Resources.p26);
+            picty.Add(Resources.p27);
+            picty.Add(Resources.p28);
+            picty.Add(Resources.p29);
+            picty.Add(Resources.p30);
+            picty.Add(Resources.p31);
+            picty.Add(Resources.p32);
+            picty.Add(Resources.p33);
+            picty.Add(Resources.p34);
+            picty.Add(Resources.p35);
+            picty.Add(Resources.p36);
+            picty.Add(Resources.p37);
+            picty.Add(Resources.p38);
+            picty.Add(Resources.p39);
+            picty.Add(Resources.p40);
+            picty.Add(Resources.p41);
+            picty.Add(Resources.p42);
+            picty.Add(Resources.p43);
+            picty.Add(Resources.p44);
+            picty.Add(Resources.p45);
+            picty.Add(Resources.p46);
+            picty.Add(Resources.p47);
+            picty.Add(Resources.p48);
+            picty.Add(Resources.p49);
+            picty.Add(Resources.p50);
+            picty.Add(Resources.p51);
+            picty.Add(Resources.p52);
+            picty.Add(Resources.p53);
+            picty.Add(Resources.p54);
+            picty.Add(Resources.p55);
+            picty.Add(Resources.p56);
+            picty.Add(Resources.p57);
+            picty.Add(Resources.p58);
+            picty.Add(Resources.p59);
+        }
+    }
+}
